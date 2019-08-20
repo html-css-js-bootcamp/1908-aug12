eecdaa94ea110e626178e0e26d5ab7b626a545ca
Pushpinder Kaur <kaurrevature@outlook.com>
2019-08-20 21:35:03 +0530

    functions and fizzbuzz program

--- scripts/MoreJs.js
+++ scripts/MoreJs.js
@@ -44,7 +44,37 @@ console.log(`${num} is odd`);// one line statements after if and else donot requ
     console.log('Sorry you fail');*/
 //HW
 // write a program for fizz buzz in JS (numbers from 1 to 100, if number is divisible by 3 say fizz, if divisible by 5 say buzz and 
-// if divisible by both 3 and 5 it should say fizzbuzz)
+// if divisible by both 3 and 5 it should say  fizzbuzz)
+
+/*function fizzbuzz(number){
+    if(number>0 && number<101){
+        if(number%3==0 && number%5==0)
+            console.log('fizzbuzz');
+        else if(number%3==0)
+            console.log('fizz');
+        else if(number%5==0)
+            console.log('buzz');
+        else
+            console.log(number);
+    }
+}
+let num=Number(prompt('Enter the number'));
+fizzbuzz(num);*/
+
+function fizzbuzz(){
+//for(initialization;condition;increment/decrement)
+    for(i=1;i<101;i++){
+        if(i%3==0 && i%5==0)
+        console.log('fizzbuzz');
+    else if(i%3==0)
+        console.log('fizz');
+    else if(i%5==0)
+        console.log('buzz');
+    else
+        console.log(i);
+    }
+}
+//fizzbuzz();
 
 // SWITCH - used for multi-condition checking but with matching pattern
 /**
@@ -118,8 +148,35 @@ while(a>0 && a<11){
     a++;
 }*/
 
+// FOR LOOP:
+// write a program in JS to write odd numbers from 1 to 100
+// function printOdd(){
+//     console.log("Printing odd numbers from 1 to 100");
+//     for(var i=1;i<101;i++){
+//         if(i%2==1)
+//             console.log(i);
+//     }
+// }
+//printOdd();
+// Write a program in JS to print number in reverse order
+// for(let i=100;i>0;i--){
+//     console.log(i);
+// }
+
+// write a program in JS to print from -1 to -20 how we do it
+
+// for(let i=-1;i>-21;i--){
+//     console.log(i);
+// }
+
+                //or
+// for(let j=-20;j<0;j++){
+//     console.log(j);
+// }
+
+//
 // FUNCTIONS: behavior of the program
-// CONVENTION: verb, camelCase or ProperCase
+// CONVENTION: name it as a verb, camelCase or ProperCase
 // Named Function
 /*function sayHello()// declaration
 {
@@ -148,11 +205,22 @@ wishHello();*/
 }
 anonymousFunctionRef();*/
 
-// SHORTHAND OF ANONYMOUS FUNCTIONS-> ARROW FUNCTIONS:
+// SHORTHAND OF ANONYMOUS FUNCTIONS IN ES6-> ARROW FUNCTIONS:
 // var arrowFuncRef= ()=> console.log('Hello Arrow functions');
 // arrowFuncRef();
 
-// FUNCTION with parameters
+// ARROW FUNCTIONS WITH PARAMETERS:
+// var add=(a,b)=>console.log(a+b);
+// add(30,60);
+
+/*var isEven=(num)=>{
+    if(num%2==0)
+        console.log(`${num} is even`);
+    else
+        console.log(`${num} is odd`);
+};
+isEven(5);*/
+// FUNCTION with parameters- to make function modular
 /*function add()
 {
     let a= Number(prompt('Enter number 1'));
@@ -160,9 +228,52 @@ anonymousFunctionRef();*/
     console.log(a+b);
 }
 add();*/
-function add(a,b){
+/*function add(a,b){
     console.log(a+b);
 }
 let a= Number(prompt('Enter number 1'));
 let b= Number(prompt('Enter number 2'));
-add(a,b);
+add(a,b);*/
+
+//FUNCTION THAT RETURNS A VALUE
+ // Named Function
+    // function add(a,b){
+    //     return a+b;
+    // }
+
+    // function isEven(num){
+    //     if(num%2==0)
+    //         return true;
+    //     else
+    //         return false;
+    // }
+    // var checkEven=isEven(7);
+    // console.log(checkEven);
+
+// Anonymous functions
+    // var add=function(a,b){
+    //     return a+b;
+    // }
+
+    // var isEven=function(num){
+    //     if(num%2==0)
+    //         return true;
+    //     else
+    //         return false;
+    // }
+    // var checkEven=isEven(70);
+    // console.log(checkEven);
+
+//Arrow functions
+    // var add=(a,b)=>a+b;
+    // var result=add(10,40);
+    // console.log(result);
+
+    var isEven=(num)=>{
+        if(num%2==0)
+            return true;
+        else 
+            return false;
+    }
+    var checkEven=isEven(65);
+    console.log(checkEven);
